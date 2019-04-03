125d9f6e21a84997d8b67b02226e1ab16c29bed2
anyahayda <35680365+anyahayda@users.noreply.github.com>
2019-04-03 21:12:11 +0300

    Update script.py

--- StringToInt/script.py
+++ StringToInt/script.py
@@ -26,12 +26,15 @@ def check_results():
 
 
 if __name__ == "__main__":
-    time = 0
+    time, times, num = 0, [], int(sys.argv[1])
     print("The minimum result of each method in microseconds:")
     for i in range(1, 4):
-        for amount in range(int(sys.argv[1])):
+        for amount in range(num):
             sub_run = subprocess.run([name, str(i), sys.argv[2], sys.argv[3]], shell=False,
                                      stdout=subprocess.PIPE)
             time = int(sub_run.stdout.decode().strip().split('\n')[-1])
+            times.append(time)
         check_results()
-        print("{} method: {}".format(i, time))
+    print("1 method: {}".format( min(times[0:num])))
+    print("2 method: {}".format( min(times[num:num*2])))
+    print("3 method: {}".format( min(times[num*2::])))
